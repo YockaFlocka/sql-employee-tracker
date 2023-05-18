37205ffb97c0e215bbf284d342e45f1a0c7043db
Peter Yockey <peterjyockey@gmail.com>
2023-05-18 16:57:42 -0500

    added initial user prompts and first function

--- index.js
+++ index.js
@@ -1,34 +1,62 @@
 const inquirer = require("inquirer");
-const fs = require("fs");
+const mysql = require('mysql2');
+const express = require('express');
+require('dotenv').config();
+const cTable = require('console.table');
 
-const questions = [
-    {
-        type: "input",
-        name: "characters",
-        message: "Type 3 characters you want for your SVG logo",
-        validate: your_title => {
-            if (your_title.length === 3) {
-                return true;
-            } else {
-                console.log(": Please enter 3 characters, no more no less.")
-                return false;
-            }
-        } 
-    },
-    {
-        type: "input",
-        name: "charcolor",
-        message: "What color do you want the text to be? Pick a color or hexidecimal number"
-    },
-    {
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+app.use(express.urlencoded({extended: false }));
+app.use(express.json());
+
+const db = mysql.createConnection(
+  {
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
+  },
+  console.log("Connected to the employee tracker database.")
+)
+
+function init() {
+    inquirer.prompt([
+      {
+        // Ask the user what option they would like to choose
         type: "list",
-        name: "shape",
-        message: "What shape do you want the logo to have?",
-        choices: ['Circle', 'Square', 'Triangle'] 
-    },
-    {
-        type: "input",
-        name: "shapecolor",
-        message: "What color do you want the shape to be? Pick a color or hexidecimal number",
-    }
-];
+        message: "Please select an option:",
+        name: 'initQuestions',
+        choices: [ "View All Departments", "View All Roles", "View All Employees", "Add A Department", "Add A Role", "Add An Employee", "Update An Employee Role" ],
+      },
+    ])
+    .then((data) => {
+      switch(data.initQuestions) {
+        case "View All Departments": viewDepartments();
+        break;
+        case "View All Roles": viewRoles();
+        break;
+        case "View All Employees": viewEmployees();
+        break;
+        case "Add A Department": addDepartment();
+        break;
+        case "Add A Role": addRole();
+        break;
+        case "Add An Employee": addEmployee();
+        break;
+        case "Update An Employee Role": updateEmployee();
+        break;
+      }
+    })
+  }
+
+  // Call the function
+  init();
+
+  // Show departments table
+  function viewDepartments() {
+    db.query("SELECT * FROM departments", function (err, results) {
+        console.table(results);
+        init();
+    });
+  }
